6c96ee31c7436bd1d1cf9fe67d6af88459876701
vaish17 <35229316+vaish17@users.noreply.github.com>
2018-01-10 21:24:38 +0530

    Update even_odd_sequence.py

--- even_odd_sequence.py
+++ even_odd_sequence.py
@@ -3,10 +3,13 @@ li = [1,6,3,5,8,2,7,4]
 size = len(li)
 a = 0
 for i in range(0,(size)):
-    if((li[i] % 2) == 0):
-        a = li[i]
-        li.remove(li[i])
-        li.insert(0,a)
+    for j in range(size,0):
+        if((li[j] % 2) == 0):
+            temp = li[i]
+            li[i] = li[j]
+            li[j] = temp
+            i += 1
+        j -= 1
 print li
 
 
